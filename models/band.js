5934259d2009d488468e90b9d462997b4f583913
Manny Delgado <mannygdp@gmail.com>
2023-09-25 21:37:45 -0700

    new pages

--- models/band.js
+++ models/band.js
@@ -9,10 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({MeetGreet}) {
       // define association here
+      Band.hasMany(MeetGreet,{
+        foreignKey: 'band_id',
+        as: 'meet_greets'
+      })
+      Band.hasMany({
+        foreignKey: 'band_id',
+        as: 'set_times'
+    })
     }
   }
+
   Band.init({
     band_id: {
       type: DataTypes.INTEGER,
